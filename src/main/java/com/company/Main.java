140f2080f8cb647e16e69d011909bbc28e17834a
Vlad-Andr <andreev.v99@gmail.com>
2019-08-22 20:06:58 +0300

    Fixed all support remarks for HTTP task

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.company.httpstatustask.ReliseHTTPApp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ReliseHTTPApp reliseHTTPApp = new ReliseHTTPApp();
+        reliseHTTPApp.reliseApp();
+    }
+}
